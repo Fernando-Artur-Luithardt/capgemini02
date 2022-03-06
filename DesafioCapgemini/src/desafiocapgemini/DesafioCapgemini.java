8fde21fb73448e6c9de6d194f45436d97dea1bc6
Fernando-Artur-Luithardt <fluithardt@gmail.com>
2022-03-06 20:59:16 -0300

    Comentando todo código

--- DesafioCapgemini/src/desafiocapgemini/DesafioCapgemini.java
+++ DesafioCapgemini/src/desafiocapgemini/DesafioCapgemini.java
@@ -12,16 +12,17 @@ public class DesafioCapgemini {
             int i = 1;
             while (i <= casasString) {
                 System.out.print("ENTRE COM O " + i + "° NÚMERO: ");
-                float lista= input.nextInt(); //adicionar um if(input = null){print("necessário entrar com um número, podendo ser 0")} se input for nulo
+                float lista= input.nextInt(); 
                 entradaNumerica.add(lista);
                 i++;
             }
+            //LÓGICA
             float resultado = 0.0f;
             int posicaoNumero;
-            if (entradaNumerica.size() %2 !=0) {
+            if (entradaNumerica.size() %2 !=0) { //SE O TAMNNHO DO VETOR FOR IMPAR A MEDIANA É O NÚMERO CENTRAL
                 posicaoNumero = ((entradaNumerica.size() - 1)/2);
                 resultado = entradaNumerica.get(posicaoNumero);
-            }else{
+            }else{  //SE O TAMNNHO DO VETOR FOR PAR A MEDIANA É OS DOIS NUMEROS CENTRAIS SOMADOS E DIVIDIDOS POR 2
                 posicaoNumero = ((entradaNumerica.size())/2);
                 resultado = ((entradaNumerica.get(posicaoNumero))+(entradaNumerica.get(posicaoNumero-1)))/2;
             }
@@ -43,13 +44,14 @@ public class DesafioCapgemini {
             }
         System.out.print("\nENTRE COM 'X' (diferença entre as casas do vetor): ");
         int x = input.nextInt();
+        //LÓGICA
+        int y;
+        int resultado = 0;
         i = 0;
-        int y = 0;
-        float resultado = 0.0f;
-        while(i < vetor.size()) {
-            y = i;
+        while(i < vetor.size()) { //DOIS LAÇOS DE REPETIÇÃO PARA CADA NUMERO SER COMPARADO COM TODOS A SUA FRENTE
+            y = i;                //y = i PARA QUE O LAÇO INTERNO INCIE A COMPARAÇÃO A PARTIR DA PRÓXIMA CASA DO VETOR CADA CICLO
             while(y < (vetor.size()-1)) {
-                if ((vetor.get(i) - x) == vetor.get(y+1)){
+                if ((vetor.get(i) - x) == vetor.get(y+1)){ //IF / ELSE IF PARA COMPARAR A DIFERENÇA ENTRE OS VALORES DO VETOR
                 resultado++;                
                 }else if ((vetor.get(i) + x) == vetor.get(y+1)){
                 resultado++;
@@ -75,14 +77,17 @@ public class DesafioCapgemini {
         int linhas = 0;
         
         System.out.print("\n RESPOSTA: ");
+        //LÓGICA
+        //SE COMPRIMENTO DA STRING NÃO FOR EXATAMENTE DIVISIVEL PELA RAIZ SIGNIFICA QUE O NÚMERO DE LINHAS É O VALOR RAIZ + 1
+        linhas = (int) raiz +1;
         if (length % raiz !=0) {
-            linhas = (int) raiz +1;
         } else {linhas = (int) raiz;}
         int caracter = 0;
-        while (i < linhas){
-            while(j <= linhas){               
-                    caracter = (int) (j * raiz) + i;
-                    if (caracter <= (entrada.length()-1)){
+        while (i < linhas){//PULA PARA LER A NOVA COLUNA A CADA CICLO
+            while(j <= linhas){//FAZ O PRINT DOS CARACTERES DE CADA COLUNA              
+                    caracter = (int) (j * raiz) + i; //SELECIONA OS CARACTERES QUE VÃO PARA TELA (j * raiz(SELECIONA OS CARACTERES DA LINHA))
+                    //+i SELECIONA A COLUNA QUE OS CARACTERES VÃO SER PEGOS
+                    if (caracter <= (entrada.length()-1)){//GARANTE QUE O NÚMERO DO CARACTER NÃO ULTRAPASSE O COMPRIMENTO DA STRING
                         System.out.print(entrada.charAt(caracter));
                     } else{
                         break;
